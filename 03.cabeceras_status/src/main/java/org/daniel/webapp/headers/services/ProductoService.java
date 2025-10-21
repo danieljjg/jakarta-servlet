0295714107055017348a008c6763db51d7961e94
danieljj28 <danijj28@gmail.com>
2025-10-21 12:07:16 +0200

    change dot for dash

--- 03.cabeceras_status/src/main/java/org/daniel/webapp/headers/services/ProductoService.java
+++
@@ -1,9 +0,0 @@
-package org.daniel.webapp.headers.services;
-
-import org.daniel.webapp.headers.models.Producto;
-
-import java.util.List;
-
-public interface ProductoService {
-    List<Producto> getAllProducts();
-}
